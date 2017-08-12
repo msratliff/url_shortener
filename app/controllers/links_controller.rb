54b98f5bf99194c697fde600f279aec628f64ad5
Mark Ratliff <mark.s.ratliff@gmail.com>
2017-08-12 18:14:19 -0400

    Links working, able to both see link page and redirect to target site

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,29 +1,52 @@
 class LinksController < ApplicationController
 
+	before_action :set_link, only: [:show]
+
+	def index
+		@links = Link.all
+	end
+
 	def new
 		@link = Link.new
 	end
 
 	def create
 		@link = Link.new(user_params)
+		@link.generate_short_url
+
 		if @link.save
-			redirect_to show_path
+			redirect_to '/links/new', notice: "Shortened URL: http://localhost:3000/#{@link.short_url}"
 		else
-			@link.generate_short_url
-			@link.save
+			render 'new'
 		end
 
 	end
 
+	def show
+		if params.include? :id
+			render 'links/show'
+		else
+			redirect_to @link.long_url
+		end
+		
+	end
+
+
 
 	private
 
-		def set_user
-    	@user = User.find(params[:id])
+		def set_link
+    		if params.include? :id
+    			@link = Link.find(params[:id])
+    		end
+
+    		if params.include? :short_url
+	    		@link = Link.find_by(short_url: params[:short_url])
+	    	end
 		end
 
 		def user_params
-			params.require(:link).permit(:long_url)
+			params.require(:link).permit(:id, :long_url, :short_url)
 		end
 
 end
